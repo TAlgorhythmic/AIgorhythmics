ed5f2e79ed0e2722741b2ed21ca23796f74ad40d
Algorhythmics <intivioli@gmail.com>
2025-06-05 22:53:34 +0200

    Update

--- src/input/buffers/audio.rs
+++ src/input/buffers/audio.rs
@@ -1,14 +1,3 @@
-use jack::{Client, ClientOptions, ClientStatus};
-
-pub struct AudioBuffer {
-    client: Client,
-    client_status: ClientStatus,
-}
-
-impl AudioBuffer {
-    pub fn new(name: &str) -> Self {
-        let (client, client_status) = Client::new(name, ClientOptions::default()).expect("Error initializing jack client");
-
-        Self { client, client_status }
-    }
+pub struct AudioContext {
+    
 }
--- src/input/senses/auditive_module.rs
+++ src/input/senses/auditive_module.rs
@@ -1,3 +1,23 @@
-pub fn listen(buf: &mut AudioBuffer) {
+use std::error::Error;
 
+use cpal::{traits::{DeviceTrait, HostTrait, StreamTrait}, FromSample, SampleRate, StreamConfig};
+
+use crate::neural_net::analyzer::audio::AudioProcessor;
+
+pub fn listen(proc: AudioProcessor) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let host = cpal::default_host();
+    
+    let input = host.default_input_device().expect("Failed to find a default input device");
+    let conf = StreamConfig { channels: 1, sample_rate: SampleRate { 0: 24000 }, buffer_size: cpal::BufferSize::Default };
+    
+    let stream = input.build_input_stream_raw(&conf, cpal::SampleFormat::F32, |data, info| {
+        println!("Data: {}", data.bytes().iter().map(|n| n.to_string()).collect::<Vec<_>>().join(" "));
+
+        todo!("Learn how this works and handle data.");
+    }, |err| {
+        
+    }, None)?;
+
+    stream.play()?;
+    Ok(())
 }
